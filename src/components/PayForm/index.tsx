bfeb2bce0c2aa938ef2d047ede849115843ca42a
Carlos <carlosgarcia_97gr@hotmail.com>
2020-01-20 18:51:09 -0500

    new styles for pays form

--- src/components/PayForm/index.tsx
+++ src/components/PayForm/index.tsx
@@ -1,51 +1,94 @@
-import React,{useState,FormEvent} from "react";
+import React, { useState, FormEvent, FunctionComponent as FC } from "react";
 import { useInputValue } from "../../hooks/useInputValue";
-import {Form} from './styles'
+import {
+  Form,
+  Title,
+  FormContainer,
+  Input,
+  Button,
+  FloattingLogo,
+  Error
+} from "./styles";
 
-export const PayForm = () => {
+export const PayForm: FC = () => {
   const title = useInputValue("");
   const content = useInputValue("");
-  const nparticipants = useInputValue(0);
-  const amount = useInputValue(0);
+  const nparticipants = useInputValue("");
+  const amount = useInputValue("");
 
-  const [seending, setSeending] = useState(false)
+  const [seending, setSeending] = useState(false);
+  const [error, SetError] = useState("");
+  const [existError, SetExistError] = useState(false);
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    
+    e.preventDefault();
+    const validate =validateForm()
+    setTimeout(() => {
+      if (validate) {
+        console.log("validado")
+        setSeending(true);
+      }
+    }, 1000);
+    setSeending(false)
+    console.log(seending)
+  };
 
-  const handleSubmit = async (e:FormEvent<HTMLFormElement>): Promise<void> =>{
-      e.preventDefault()
-      setTimeout(() => {
-        setSeending(true)
-        console.log(seending)
-      }, 2000)
-      
+  const validateForm = () => {
+    if (title.value === "") {
+      SetError("Add a title");
+      SetExistError(true);
+      return false;
+    }
+    if (content.value === "") {
+      SetError("Add Content");
+      SetExistError(true);
+      return false;
+    }
+    if (nparticipants.value === "") {
+      SetError("Add the number of participants");
+      SetExistError(true);
+      return false;
+    }
+    if (amount.value === "") {
+      SetError("Add a valid amount");
+      SetExistError(true);
+      return false;
+    }
+    return true;
   };
   return (
-    <Form onSubmit={handleSubmit}>
-      <input
-        placeholder="Title"
-        value={title.value}
-        onChange={title.onChange}
-        type="text"
-      />
-      <input
-        placeholder="Description"
-        value={content.value}
-        onChange={content.onChange}
-        type="text"
-      />
-      <input
-        placeholder="Number of participants"
-        value={nparticipants.value}
-        onChange={nparticipants.onChange}
-        type="number"
-      />
-      <input
-        placeholder="Amount in Dollars"
-        value={amount.value}
-        onChange={amount.onChange}
-        type="number"
-      />
-      <button>Enviar</button>
-    </Form>
+    <FormContainer>
+      <FloattingLogo></FloattingLogo>
+      <Title>${amount.value}</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Title"
+          value={title.value}
+          onChange={title.onChange}
+          type="text"
+        />
+        <Input
+          placeholder="Description"
+          value={content.value}
+          onChange={content.onChange}
+          type="text"
+        />
+        <Input
+          placeholder="Number of participants"
+          value={nparticipants.value}
+          onChange={nparticipants.onChange}
+          type="number"
+        />
+        <Input
+          placeholder="Amount in Dollars"
+          value={amount.value}
+          onChange={amount.onChange}
+          type="number"
+        />
+        <Button>Add Pay </Button>
+        {existError && <Error>{error}</Error>}
+      </Form>
+    </FormContainer>
   );
 };
--- src/components/PayForm/styles.js
+++ src/components/PayForm/styles.js
@@ -1,7 +1,128 @@
 import styled from "styled-components";
+import { Logo } from "../Logo";
+import {fadeIn} from '../../styles/animation'
 
 export const Form = styled.form`
-display:flex;
-flex-direction:column;
-padding:10px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`;
+export const Title = styled.div`
+  background: rgb(66, 103, 178);
+  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.3);
+  color: rgb(66, 103, 178);
+  font-weight: 600;
+  font-size: 25px;
+  padding: 35px;
+  color: white;
+  border-radius: 0px 0px 50px 50px;
+  text-align: right;
+  &:after {
+    background-color: rgba(201, 145, 26, 0.9);
+    content: "blah";
+    width: 101px;
+    height: 101px;
+    color: transparent;
+    border-radius: 0% 50% 50% 50%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+export const FormContainer = styled.div`
+  padding: 0px;
+`;
+
+export const Input = styled.input`
+  padding: 10px;
+  margin: 10px 0px;
+  border: 2px solid rgb(41, 72, 125);
+  border-radius: 20px;
+  &:focus {
+    outline: none;
+    border: 2px solid #c9911a;
+    border-radius: 20px;
+    box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.3);
+  }
+`;
+export const Button = styled.button`
+  background-image: linear-gradient(45deg, rgb(66, 103, 178), rgb(41, 72, 125));
+  padding: 10px;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3);
+  border-left: 10px solid #c9911a;
+  &:hover {
+    border-left: 10px solid rgb(41, 72, 125);
+  }
+`;
+export const FloattingLogo = styled(Logo)`
+  position: absolute;
+  top: 10px;
+  left: 0;
+  width: 100px;
+  z-index: 9999;
+`;
+
+export const InputRange = styled.input`
+  border: none;
+  width: 100%;
+  background: transparent;
+  padding:20px;
+  &:-webkit-slider-thumb {
+    -webkit-appearance: none;
+  }
+  &:-ms-track {
+    width: 100%;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  &:focus {
+    outline: none;
+  }
+  &:-webkit-slider-thumb {
+    -webkit-appearance: none;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+    margin-top: -14px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  &:-moz-range-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+  &:-ms-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+`;
+
+export const Error = styled.span`
+color: white;
+margin-top:10px;
+padding:10px 0px;
+border-radius:6px;
+width:100%;
+text-align:center;
+background-image: linear-gradient(45deg, rgb(211, 47, 47), rgb(154, 0, 7));
+${fadeIn({time:"0.1s"})}
 `
